cde38f5fab5faa0d2f96d302308f4c2cd39019b6
upit <upit.upit@enigma.com>
2025-08-30 10:55:34 +0700

    [revisi - model Pegawai

--- backend/contract-application/src/main/java/com/app/model/Pegawai.java
+++ backend/contract-application/src/main/java/com/app/model/Pegawai.java
@@ -1,5 +1,6 @@
 package com.app.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,12 +23,17 @@ public class Pegawai {
     private String kodePegawai;
     @Column(nullable = false)
     private String namaPegawai;
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_jabatan", nullable = false)
+    @JsonIgnoreProperties("pegawaiList")
     private Jabatan jabatan;
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_cabang", nullable = false)
+    @JsonIgnoreProperties("pegawaiList")
     private Cabang cabang;
+
     @Column(nullable = false)
     private LocalDate tanggalMulaiKontrak;
     @Column(nullable = false)
